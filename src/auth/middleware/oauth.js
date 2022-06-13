fe5d25bd501f2b064e9d29ac3d0ada61d4a17f50
Esam-Ankir <eankir@hotmail.com>
2022-06-13 19:11:22 +0300

    edit db name

---
+++ src/auth/middleware/oauth.js
@@ -0,0 +1,4 @@
+let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'sdkjdsljd=']
+let encodedString = basicHeaderParts.pop();  // sdkjdsljd=
+let decodedString = base64.decode(encodedString); // "username:password"
+let [username, password] = decodedString.split(':'); // username, password
