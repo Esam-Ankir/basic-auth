cd68416f6a5a42287b7b0041b739576c9b4a91bf
Esam-Ankir <eankir@hotmail.com>
2022-06-13 18:30:28 +0300

    Authentication Done

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+require("dotenv").config();
+let PORT = process.env.PORT || 3000;
+const server = require("./src/server");
+
+const { db } = require("./src/auth/models/index-model");
+
+db.sync()
+    .then(() => {
+        server.start(PORT);
+    })
+    .catch(console.error);
